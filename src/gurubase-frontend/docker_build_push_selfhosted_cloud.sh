903c7063679943638fc57b1aa44ab42ac5a216ac
fatihbaltaci <baltacifatih14@gmail.com>
2025-01-21 17:53:29 +0300

    Update versions

--- src/gurubase-frontend/docker_build_push_selfhosted_cloud.sh
+++ src/gurubase-frontend/docker_build_push_selfhosted_cloud.sh
@@ -5,7 +5,7 @@
 # Get version as argument and default to latest
 VERSION=${1:-latest-selfhosted}
 PLATFORM=${2:-linux/amd64,linux/arm64}
-DOCKER_IMAGE_TAG=ddosify/kubernetesguru_frontend:$VERSION
+# DOCKER_IMAGE_TAG=ddosify/kubernetesguru_frontend:$VERSION
 DOCKER_IMAGE_TAG2=ddosify/gurubase-frontend:$VERSION
 DOCKERFILE=Dockerfile.selfhosted
 
@@ -13,4 +13,6 @@ echo "Building $DOCKER_IMAGE_TAG"
 
 docker buildx create --driver cloud ddosify/builder
 
-docker buildx build --push --platform=$PLATFORM --builder=cloud-ddosify-builder --tag=$DOCKER_IMAGE_TAG --tag=$DOCKER_IMAGE_TAG2 -f $DOCKERFILE .
+# docker buildx build --push --platform=$PLATFORM --builder=cloud-ddosify-builder --tag=$DOCKER_IMAGE_TAG --tag=$DOCKER_IMAGE_TAG2 -f $DOCKERFILE .
+
+docker buildx build --push --platform=$PLATFORM --builder=cloud-ddosify-builder --tag=$DOCKER_IMAGE_TAG2 -f $DOCKERFILE .
