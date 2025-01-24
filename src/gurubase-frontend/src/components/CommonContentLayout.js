7690d8fedf8bf5bd61dc7c8943df1ff96c547d07
Aral Yekta <aralyektayarimca@gmail.com>
2025-01-24 14:31:59 +0000

    Fix sidebar for large screens

--- src/gurubase-frontend/src/components/CommonContentLayout.js
+++ src/gurubase-frontend/src/components/CommonContentLayout.js
@@ -3,9 +3,9 @@ import React from "react";
 const CommonContentLayout = ({ children, sidebar }) => {
   return (
     <main className="z-10 flex justify-center items-center px-16 xs:px-0 w-full flex-grow xs:max-w-full polygon-fill">
-      <div className="flex justify-between w-full gap-4 h-full">
+      <div className="flex max-w-[1440px] w-full gap-4 h-full">
         {sidebar && <div className="h-full pt-10">{sidebar}</div>}
-        <section className="container mx-auto max-w-[1180px] bg-white h-full xs:border-none border-l border-r border-solid border-neutral-200">
+        <section className="flex-1 bg-white h-full xs:border-none border-l border-r border-solid border-neutral-200">
           <div className="flex h-full gap-6">
             <section className="flex flex-col flex-grow w-full h-full xs:ml-0">
               {/* Content goes here */}
