e4371c26cc1a541e6df16c8eeba78ec4b995ecde
Aral Yekta <aralyektayarimca@gmail.com>
2025-01-24 17:34:07 +0300

    Ensure login on integrations page

--- src/gurubase-frontend/src/app/guru/[customGuru]/integrations/page.js
+++ src/gurubase-frontend/src/app/guru/[customGuru]/integrations/page.js
@@ -1,10 +1,10 @@
-import { getGuruTypes } from "@/app/actions";
+import { getMyGurus } from "@/app/actions";
 import IntegrationPayeLayout from "@/components/Integrations/IntegrationPayeLayout";
 import IntegrationTypesList from "@/components/Integrations/IntegrationTypesList";
 
 export default async function IntegrationsPage({ params }) {
   const { customGuru } = params;
-  const guruTypes = await getGuruTypes();
+  const guruTypes = await getMyGurus();
 
   return (
     <IntegrationPayeLayout
