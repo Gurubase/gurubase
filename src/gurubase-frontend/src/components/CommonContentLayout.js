c564e454213a3bf19bacfd6712e213760df639a6
Aral Yekta <aralyektayarimca@gmail.com>
2025-01-31 14:20:25 +0300

    Fix header-footer spacing for privacy policy etc. pages

--- src/gurubase-frontend/src/components/CommonContentLayout.js
+++ src/gurubase-frontend/src/components/CommonContentLayout.js
@@ -1,9 +1,16 @@
+import clsx from "clsx";
 import React from "react";
 
 const CommonContentLayout = ({ children, sidebar }) => {
   return (
     <main className="z-10 flex justify-center w-full flex-grow guru-sm:px-0 px-8">
-      <div className="flex guru-sm:flex-col gap-6 guru-sm:gap-0 h-full w-full max-w-[1440px]">
+      <div
+        className={clsx(
+          "flex guru-sm:flex-col gap-6 guru-sm:gap-0 h-full w-full",
+          sidebar
+            ? "max-w-[1440px]"
+            : "guru-md:max-w-[870px] guru-lg:max-w-[1180px]"
+        )}>
         {sidebar}
         <section className="flex-1 bg-white h-full guru-sm:border-0 border-l border-r border-solid border-neutral-200">
           <div className="flex h-full">
