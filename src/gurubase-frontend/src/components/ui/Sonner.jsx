97fb9471e4e41f76a4ad163f5fa5ebd64cab6497
Aral Yekta <aralyektayarimca@gmail.com>
2025-01-31 13:22:13 +0300

    Fix toast colors

--- src/gurubase-frontend/src/components/ui/Sonner.jsx
+++ src/gurubase-frontend/src/components/ui/Sonner.jsx
@@ -6,8 +6,7 @@ export function Toaster({ ...props }) {
       className="toaster group mb-20 !z-[99999]" // Use !z-[99999] to force a very high z-index
       toastOptions={{
         classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          toast: "group toast group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
