c1f795b8ad224e8ef53c3fba4bd999cdd8ec9a48
Aral Yekta <aralyektayarimca@gmail.com>
2025-02-05 17:07:22 +0300

    Fix analytics on selfhosted

--- src/gurubase-frontend/src/app/guru/[customGuru]/analytics/page.js
+++ src/gurubase-frontend/src/app/guru/[customGuru]/analytics/page.js
@@ -5,16 +5,8 @@ import AnalyticsPageLayout from "@/components/Analytics/AnalyticsPageLayout";
 
 export default async function AnalyticsPage({ params, searchParams }) {
   const { customGuru } = params;
-  const hasError = searchParams && searchParams.error === "true";
 
   const guruTypes = await getMyGurus();
-  const currentGuru = guruTypes.find((guru) => guru.slug === customGuru);
-
-  const selfhosted = process.env.NEXT_PUBLIC_NODE_ENV === "selfhosted";
-
-  if (selfhosted) {
-    redirect("/not-found");
-  }
 
   return <AnalyticsPageLayout customGuru={customGuru} guruTypes={guruTypes} />;
 }
