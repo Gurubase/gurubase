36108516c8aa60f045adaf0235b9597c2ad491d2
Kursat Aktas <kursat.ce@gmail.com>
2025-02-24 16:21:22 +0300

    add periodic task run into the start celery beat

--- src/gurubase-backend/start_scripts/start_celery_beat.sh
+++ src/gurubase-backend/start_scripts/start_celery_beat.sh
@@ -3,4 +3,6 @@
 set -o errexit
 set -o nounset
 
-cd backend && exec celery --app backend beat --pidfile= --scheduler django_celery_beat.schedulers:DatabaseScheduler --loglevel=INFO
+cd backend && \
+python scripts/2_generate_periodic_tasks.py && \
+exec celery --app backend beat --pidfile= --scheduler django_celery_beat.schedulers:DatabaseScheduler --loglevel=INFO
