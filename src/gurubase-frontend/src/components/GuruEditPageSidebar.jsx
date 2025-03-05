bf787bad524d4777614f76f375034074b7a1c05b
aralyekta <aralyektayarimca@gmail.com>
2025-03-05 14:02:11 +0300

    Add "Visit guru" button to guru edit page

--- src/gurubase-frontend/src/components/GuruEditPageSidebar.jsx
+++ src/gurubase-frontend/src/components/GuruEditPageSidebar.jsx
@@ -54,7 +54,7 @@ export default function GuruEditPageSidebar({ guruData }) {
       <div className="sticky guru-sm:static top-[100px]">
         <nav className="flex flex-col items-center w-[250px] guru-sm:w-full guru-sm:mx-auto guru-sm:mb-5 mb-0 bg-white guru-sm:border-0 border rounded-lg border-[#E2E2E2]">
           {/* Guru Logo and Name */}
-          <div className="flex items-center gap-2 w-full p-5 guru-sm:hidden">
+          <div className="flex items-center gap-2 w-full pb-3 pt-5 px-5 guru-sm:hidden">
             {guruLogo && (
               <div className="w-8 h-8 rounded-lg flex items-center justify-center overflow-hidden">
                 <Image
@@ -69,6 +69,32 @@ export default function GuruEditPageSidebar({ guruData }) {
             <span className="font-inter text-[14px] font-medium leading-[20px] text-[#191919]">
               {guruName} Guru
             </span>
+          </div>
+
+          {/* Visit Guru Button */}
+          <div className="w-full px-5 pb-5 guru-sm:hidden">
+            <Button
+              variant="outline"
+              size="smButtonLgText"
+              className="w-full text-black hover:bg-gray-800 hover:text-white rounded-full"
+              onClick={() => handleNavigation(`/g/${guruSlug}`)}>
+              <div className="inline-flex items-center gap-1">
+                <span>Visit Guru</span>
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M6 4.5C5.72386 4.5 5.5 4.27614 5.5 4C5.5 3.72386 5.72386 3.5 6 3.5H12C12.2761 3.5 12.5 3.72386 12.5 4V10C12.5 10.2761 12.2761 10.5 12 10.5C11.7239 10.5 11.5 10.2761 11.5 10V5.20711L4.35355 12.3536C4.15829 12.5488 3.84171 12.5488 3.64645 12.3536C3.45118 12.1583 3.45118 11.8417 3.64645 11.6464L10.7929 4.5H6Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </div>
+            </Button>
           </div>
 
           {/* Divider */}
