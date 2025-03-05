102ab68628f14d991205ae246638156e9cc5a979
Kursat Aktas <kursat.ce@gmail.com>
2025-03-06 00:12:01 +0300

    reorder migrations

--- src/gurubase-backend/backend/core/migrations/0059_gurucreationform_source.py
+++ src/gurubase-backend/backend/core/migrations/0059_gurucreationform_source.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0057_gurucreationform'),
+        ('core', '0058_gurucreationform'),
     ]
 
     operations = [
