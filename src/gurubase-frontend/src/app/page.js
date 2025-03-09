d1737fe32cbc0f17ebf762c9cfb2eb9917ad1e76
Kursat Aktas <kursat.ce@gmail.com>
2025-03-09 21:34:48 +0000

    Add test commit

--- src/gurubase-frontend/src/app/page.js
+++ src/gurubase-frontend/src/app/page.js
@@ -3,14 +3,14 @@ import { getGuruTypes } from "@/app/actions";
 import HomePageClient from "@/components/HomePageClient";
 
 export const revalidate = 60;
-
+export const dynamic = "force-dynamic";
 const Home = async () => {
   // get guru types
   const allGuruTypes = await getGuruTypes();
 
   const readyGuruTypes = Array.isArray(allGuruTypes)
     ? // ? allGuruTypes.filter((guruType) => guruType.ready === true)
-      allGuruTypes
+    allGuruTypes
     : [];
 
   return <HomePageClient allGuruTypes={readyGuruTypes} />;
