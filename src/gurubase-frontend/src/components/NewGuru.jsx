4e27569bbc05384c16486069bd72ee44df12ebf9
aralyekta <aralyektayarimca@gmail.com>
2025-04-16 13:05:01 +0000

    Separate component

--- src/gurubase-frontend/src/components/NewGuru.jsx
+++ src/gurubase-frontend/src/components/NewGuru.jsx
@@ -108,6 +108,7 @@ import {
 } from "./ui/tooltip";
 import { HeaderTooltip } from "@/components/ui/header-tooltip";
 import { useCrawler } from "@/hooks/useCrawler";
+import { DeleteConfirmationModal } from "@/components/NewEditGuru/DeleteConfirmationModal";
 
 const formSchema = z.object({
   guruName: z
@@ -3105,41 +3106,6 @@ export default function NewGuru({ guruData, isProcessing }) {
     </>
   );
 }
-
-const DeleteConfirmationModal = ({ isOpen, onOpenChange, onDelete }) => {
-  return (
-    <Dialog open={isOpen} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-[400px] p-0">
-        <div className="p-6 text-center">
-          <DialogHeader>
-            <div className="mx-auto mb-4 h-[60px] w-[60px] rounded-full text-gray-600">
-              <SolarTrashBinTrashBold className="h-full w-full" />
-            </div>
-            <DialogTitle className="text-base font-semibold text-center text-[#191919] font-inter">
-              You are about to remove the Guru
-            </DialogTitle>
-            <DialogDescription className="text-[14px] text-[#6D6D6D] text-center font-inter font-normal">
-              If you confirm, the Guru will be removed.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="mt-6 flex flex-col gap-2">
-            <Button
-              className="h-12 px-6 justify-center items-center rounded-lg bg-[#DC2626] hover:bg-red-700 text-white"
-              onClick={onDelete}>
-              Delete
-            </Button>
-            <Button
-              className="h-12 px-4 justify-center items-center rounded-lg border border-[#1B242D] bg-white"
-              variant="outline"
-              onClick={() => onOpenChange(false)}>
-              Close
-            </Button>
-          </div>
-        </div>
-      </DialogContent>
-    </Dialog>
-  );
-};
 
 // Modal to prompt user to configure Jira integration
 const JiraIntegrationModal = ({ isOpen, onOpenChange, guruSlug }) => {
