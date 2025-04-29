121a8ff3a18a8754ec314329d65052e60f102b1d
aralyekta <aralyektayarimca@gmail.com>
2025-04-29 07:35:11 +0000

    Move rerank boolean to guru type

--- src/gurubase-backend/backend/core/utils.py
+++ src/gurubase-backend/backend/core/utils.py
@@ -565,7 +565,7 @@ def vector_db_fetch(
 
     def rerank_batch(batch, question, user_question, enhanced_question, llm_eval):
         default_settings = get_default_settings()
-        if settings.ENV == 'selfhosted' or not default_settings.rerank:
+        if settings.ENV == 'selfhosted' or not guru_type.rerank:
             # Do not rerank in selfhosted
             return [i for i in range(len(batch))], [1 for _ in range(len(batch))]
         batch_texts = [result['entity']['text'] for result in batch]
