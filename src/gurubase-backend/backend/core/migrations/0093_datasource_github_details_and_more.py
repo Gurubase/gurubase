b9e86cef0225fe71267f6955fa5ed24817cc193d
aralyekta <aralyektayarimca@gmail.com>
2025-05-08 14:23:07 +0000

    Order migrations

--- src/gurubase-backend/backend/core/migrations/0093_datasource_github_details_and_more.py
+++ src/gurubase-backend/backend/core/migrations/0093_datasource_github_details_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0081_remove_gurutype_github_repos_and_more'),
+        ('core', '0092_remove_gurutype_github_repos_and_more'),
     ]
 
     operations = [
